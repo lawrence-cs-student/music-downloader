661029845299744878cd64e045067b899bbd5329
lawrence-cs-student <114126573+lawrence-cs-student@users.noreply.github.com>
2023-09-02 23:01:25 +0800

    Update Songs.jsx

--- music-downloader-client/src/components/Songs.jsx
+++ music-downloader-client/src/components/Songs.jsx
@@ -7,7 +7,7 @@ export default function Song(props) {
         <div className="song-container">
             <div></div>
             <div>
-                <p>{data.title} dsdsd</p>
+                <p>{data.title}</p>
                 <a href={data.link}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path fill="#699bf7" d="M5 20h14v-2H5m14-9h-4V3H9v6H5l7 7l7-7Z"/></svg>
                 </a>
